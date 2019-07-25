406d7aec3138fffebe057ace84934f5738c8ca35
Irene Lopez Garcia <irene.lopez.garcia@uni-weimar.de>
2019-07-26 00:48:04 +0200

    Aufgabe 7.1 - Test other sets

--- tests/scene_tests.cpp
+++ tests/scene_tests.cpp
@@ -1,8 +1,13 @@
 #define CATCH_CONFIG_RUNNER
 
 // Project files
+#include <box.hpp>
+#include <camera.hpp>
 #include <material.hpp>
+#include <light.hpp>
 #include <scene.hpp>
+#include <shape.hpp>
+#include <sphere.hpp>
 
 // External libraries
 #include <catch.hpp>
@@ -23,6 +28,7 @@ TEST_CASE ("scene_default", "[scene]")
 // Create a new Scene object with given materials
 TEST_CASE ("scene_user_defined", "[scene]")
 {
+  // Create a set of materials
   std::set<std::shared_ptr<Material>> materials;
 
   auto material_blue = std::make_shared<Material>(
@@ -43,14 +49,34 @@ TEST_CASE ("scene_user_defined", "[scene]")
   );
   materials.insert(material_red);
 
-  // Create scene using the materials set
-  Scene scene_user_defined{materials};
+  // Create a set of shapes
+  auto sp0 = std::make_shared<Sphere>("sphere0", material_blue, glm::vec3{-10.f, -10.f, 10.f}, 5.f);
+  auto bx0 = std::make_shared<Box>("box0", material_red, glm::vec3{-10.f, -10.f, 10.f}, glm::vec3{5.f, 0.f, 20.f});
+  std::set<std::shared_ptr<Shape>> objects = {sp0, bx0};
 
-  std::shared_ptr<Material> search_dummy = std::make_shared<Material>("red", Color{}, Color{}, Color{}, 0.0f);
-  auto it = scene_user_defined.materials.find(search_dummy);
+  // Create a set of lights
+  auto light = std::make_shared<Light>(Light{"light0"});
+  std::set<std::shared_ptr<Light>> lights = {light};
+
+  // Create a set of cameras
+  auto camera = std::make_shared<Camera>(Camera{"camera0"});
+  std::set<std::shared_ptr<Camera>> cameras = {camera};
+
+
+  // Create scene using the sets
+  Scene scene_user_defined{materials, objects, lights, cameras};
+
+  // Test operator< by seaching for a material
+  auto search_dummy_material = std::make_shared<Material>("red", Color{}, Color{}, Color{}, 0.0f);
+  auto it_material = scene_user_defined.materials.find(search_dummy_material);
+
+  // Test operator< by seaching for a shape objects
+  auto search_dummy_shape = std::make_shared<Box>("box0", material_red, glm::vec3{}, glm::vec3{});
+  auto it_shape = scene_user_defined.objects.find(search_dummy_shape);
 
   REQUIRE(scene_user_defined.materials.size() == 2);
-  std::cout << "Ka material red: " << (*it)->ka << std::endl;
+  std::cout << "Ka material red: " << (*it_material)->ka << std::endl;
+  (*it_shape)->print(std::cout);
 }
 
 int main(int argc, char *argv[])
