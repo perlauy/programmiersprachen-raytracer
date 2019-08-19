70330a8abd8e8aa3bbd361429c541f501e50eb78
Irene Lopez Garcia <irene.lopez.garcia@uni-weimar.de>
2019-08-19 22:06:26 +0200

    Aufgabe 7.6 - Composite class

---
+++ framework/composite.cpp
@@ -0,0 +1,65 @@
+#include "composite.hpp"
+
+
+Composite::Composite() :
+  Shape()
+{}
+
+Composite::Composite(std::string const& name) :
+  Shape(name, nullptr)
+{}
+
+Composite::~Composite() {}
+
+float Composite::area() const {
+  float area = 0.0f;
+  for (auto child : children_) {
+    area += child->area();
+  }
+  return area;
+}
+
+float Composite::volume() const {
+  float volume = 0.0f;
+  for (auto child : children_) {
+    volume += child->volume();
+  }
+  return volume;
+}
+
+std::ostream& Composite::print(std::ostream& os) const {
+  Shape::print(os);
+  os << "COMPOSITE: " << name_ << "\n";
+  for (auto child : children_) {
+    child->print(os);
+  };
+  os << "-----------------\n";
+  return os;
+}
+
+HitPoint Composite::intersect(Ray const& original_ray) const {
+	HitPoint result{};
+	Ray transformed_ray{
+	  transform_point(Shape::world_transformation_inv_, original_ray.origin),
+	  transform_vector(Shape::world_transformation_inv_, original_ray.direction)
+	};
+
+	for (auto child : children_) {
+		HitPoint hp = child->intersect(transformed_ray);
+		if (hp.t < result.t) {
+			result = hp;
+		};
+	};
+
+	return result;
+};
+
+std::vector<std::shared_ptr<Shape>> Composite::get_children() const {
+	return children_;
+};
+
+glm::vec3 Composite::get_normal(glm::vec3 const& world_point) const {
+	// Remove this method, since it will be included in HitPoint struct
+	return {};
+};
+
