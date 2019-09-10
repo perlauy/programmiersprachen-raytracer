c017721e1f5847130d3081a0d720f5d18a15da60
Irene Lopez Garcia <irene.lopez.garcia@uni-weimar.de>
2019-09-10 18:40:10 +0200

    Presentation - Animation

--- source/presentation.cpp
+++ source/presentation.cpp
@@ -16,8 +16,8 @@
 
 int main(int argc, char* argv[])
 {
-  unsigned const FPS = 10;
-  std::string const VERSION = "003";
+  unsigned const FPS = 24;
+  std::string const VERSION = "008";
 
   std::string const output_path = "../../output/presentation/render/" + VERSION + "/";
   std::string sdf_output_path = "../../output/presentation/scene/" + VERSION + "/";
@@ -50,19 +50,19 @@ int main(int argc, char* argv[])
   );
   auto glass = std::make_shared<Material>(
     "glass",
-    Color{0.0f, 0.0f, 0.0f},
-    Color{0.9f, 1.0f, 1.0f},
-    Color{0.5f, 0.5f, 0.5f},
-    1.0f,
+    Color{0.0f, 0.0f, 0.05f},
+    Color{0.4f, 0.4f, 0.35f},
+    Color{1.0f, 1.0f, 0.9f},
+    100.0f,
     1.4f,
     0.4f
   );
   auto dark = std::make_shared<Material>(
     "dark",
     Color{0.0f, 0.0f, 0.0f},
-    Color{0.0f, 0.3f, 0.5f},
-    Color{0.0f, 0.5f, 1.0f},
-    20.0f,
+    Color{0.0f, 0.04f, 0.06f},
+    Color{0.0f, 0.8f, 0.95f},
+    30.0f,
     1.0f,
     1.0f
   );
@@ -78,36 +78,36 @@ int main(int argc, char* argv[])
   );
   auto ca = std::make_shared<Material>(
     "ca",
-    Color{0.0f, 0.0f, 1.0f},
-    Color{0.0f, 0.0f, 1.0f},
-    Color{0.0f, 0.0f, 1.0f},
+    Color{0.2f, 0.3f, 0.9f},
+    Color{0.2f, 0.3f, 0.9f},
+    Color{0.2f, 0.3f, 0.9f},
     10.0f,
     1.0f,
     1.0f
   );
   auto cb = std::make_shared<Material>(
     "cb",
-    Color{0.5f, 0.5f, 0.5f},
-    Color{0.5f, 0.5f, 0.5f},
-    Color{0.5f, 0.5f, 0.5f},
+    Color{0.95f, 0.95f, 0.2f},
+    Color{0.95f, 0.95f, 0.2f},
+    Color{0.95f, 0.95f, 0.2f},
     10.0f,
     1.0f,
     1.0f
   );
   auto cc = std::make_shared<Material>(
     "cc",
-    Color{0.9f, 0.7f, 0.0f},
-    Color{0.9f, 0.7f, 0.0f},
-    Color{0.9f, 0.7f, 0.0f},
+    Color{1.0f, 0.95f, 0.1f},
+    Color{1.0f, 0.95f, 0.1f},
+    Color{1.0f, 0.95f, 0.1f},
     10.0f,
     1.0f,
     1.0f
   );
   auto cd = std::make_shared<Material>(
     "cd",
-    Color{0.9f, 0.2f, 0.2f},
-    Color{0.9f, 0.2f, 0.2f},
-    Color{0.9f, 0.2f, 0.2f},
+    Color{0.15f, 0.3f, 0.9f},
+    Color{0.15f, 0.3f, 0.9f},
+    Color{0.15f, 0.3f, 0.9f},
     10.0f,
     1.4f,
     1.0f
@@ -115,6 +115,7 @@ int main(int argc, char* argv[])
   materials.insert({"glass", glass});
   materials.insert({"green", green});
   materials.insert({"dark", dark});
+  materials.insert({"white", white});
   materials.insert({"ca", ca});
   materials.insert({"cb", cb});
   materials.insert({"cc", cc});
@@ -122,10 +123,10 @@ int main(int argc, char* argv[])
 
   // Create shapes
   // (will animate with transform strings)
-  Box s1{"s1", ca, {-10, -10, -10}, {10, 10, 10} };
-  Box s2{"s2", cb, {-10, -10, -10}, {10, 10, 10} };
-  Box s3{"s3", ca, {-10, -10, -10}, {10, 10, 10} };
-  Box s4{"s4", ca, {-10, -10, -10}, {10, 10, 10} };
+  Box s1{"s1", ca, {-20, -20, -20}, {0, 20, 0} };
+  Box s2{"s2", cb, {-20, -20, 0}, {0, 20, 20} };
+  Box s3{"s3", cc, {0, -20, -20}, {20, 20, 0} };
+  Box s4{"s4", cd, {0, -20, 0}, {20, 20, 20} };
   auto s1_ptr = std::make_shared<Box>(s1);
   auto s2_ptr = std::make_shared<Box>(s2);
   auto s3_ptr = std::make_shared<Box>(s3);
@@ -134,26 +135,35 @@ int main(int argc, char* argv[])
   shapes.push_back(s2_ptr);
   shapes.push_back(s3_ptr);
   shapes.push_back(s4_ptr);
+  Composite cube{"cube"};
+  cube.add(s1_ptr);
+  cube.add(s2_ptr);
+  cube.add(s3_ptr);
+  cube.add(s4_ptr);
+  auto cube_ptr = std::make_shared<Composite>(cube);
+  shapes.push_back(cube_ptr);
 
-  Sphere glass_ball{"glass_ball", white, glm::vec3{0.0f,0.0f,-100.0f}, 5.0f};
-  Sphere opaque_ball{"opaque_ball", dark, glm::vec3{0.0f,0.0f,-100.0f}, 5.0f};
+  Sphere glass_ball{"glass_ball", glass, glm::vec3{0.0f,1.0f,-60.0f}, 10.0f};
   auto glass_ball_ptr = std::make_shared<Sphere>(glass_ball);
   shapes.push_back(glass_ball_ptr);
+  Sphere opaque_ball{"opaque_ball", dark, glm::vec3{0.0f,-2.0f,-30.0f}, 11.0f};
+  auto opaque_ball_ptr = std::make_shared<Sphere>(opaque_ball);
+  shapes.push_back(opaque_ball_ptr);
 
   // Create lights
   Light back_light{
     "back_light",
-    glm::vec3{0.0f, 0.0f, -120.0f},
-    Color{1.0f, 1.0f, 0.9f},
-    5.0f
+    glm::vec3{0.0f, -50.0f, -120.0f},
+    Color{1.0f, 0.95f, 0.9f},
+    1.0f
   };
   lights.push_back(back_light);
 
   Light sun{
     "sun",
-    glm::vec3{100.0f, 700.0f, -0.0f},
-    Color{0.2f, 0.5f, 0.2f},
-    5.0f
+    glm::vec3{0.0f, 700.0f, 0.0f},
+    Color{0.5f, 0.5f, 0.5f},
+    6.0f
   };
   lights.push_back(sun);
 
@@ -168,24 +178,34 @@ int main(int argc, char* argv[])
   };
   auto camera_ptr = std::make_shared<Camera>(eye);
 
-  transform_strings.push_back("transform s1 translate -10 20 -100");
-  transform_strings.push_back("transform s2 translate 10 20 -120");
-  transform_strings.push_back("transform s3 translate -10 20 -120");
-  transform_strings.push_back("transform s4 translate 10 20 -100");
   transform_strings.push_back("transform opaque_ball translate 0 0 0");
-  
-  RenderInformation info{camera_ptr, "", 800, 600};
+  transform_strings.push_back("transform glass_ball translate 0 0 0");
+  transform_strings.push_back("transform cube translate 0 0 -200");
+  transform_strings.push_back("transform cube rotate 0 0 1 0");
+  transform_strings.push_back("transform cube rotate 25 1 0 0");
+
+  RenderInformation info{camera_ptr, "", 960, 540};
  
-  float move_y = 0.0f;
+  float move_y_opaque = 0.0f;
+  float move_y_glass = 0.0f;
+  float rotate_cube = 0.0f;
   // SAVE SDF
-  for (int t = 0; t / FPS < 5.0f; ++t) {
+  for (int t = 0; t / FPS < 10.0f; ++t) {
     // Move camera upwards
-    camera_ptr->position[1] += std::sin(t / 10.0f) * 10;
+    // 1 cicle = 5 seconds = FPS * 5
+    camera_ptr->position[2] -= std::sin(t / (FPS * 5.0f)) * 0.01f;
     
     // Move ball a bit downwards
-    move_y -= std::sin(t / 10.0f) * 5;
-    transform_strings.pop_back()
-    transform_strings.push_back("transform opaque_ball translate 0 " + move_y + " 0");
+	  move_y_opaque -= std::sin(t / (FPS * 5.0f)) * 2;
+	  move_y_glass += std::sin(t / (FPS * 5.0f)) * std::sin(t / (FPS * 5.0f)) * 0.4;
+	  transform_strings.clear();
+    transform_strings.push_back("transform opaque_ball translate 0 " + std::to_string(move_y_opaque) + " 0");
+    transform_strings.push_back("transform glass_ball translate 0 " + std::to_string(move_y_glass) + " 0");
+
+    rotate_cube += t * 0.2;
+    transform_strings.push_back("transform cube translate 0 0 -200");
+    transform_strings.push_back("transform cube rotate " + std::to_string(rotate_cube) + " 0 1 0");
+    transform_strings.push_back("transform cube rotate 15 1 0 0");
 
     std::map<std::string, Camera> cameras{};
     cameras.insert({"eye", *camera_ptr});
@@ -211,7 +231,7 @@ int main(int argc, char* argv[])
   }
 
   // BATCH RENDER
-  for (int t = 0; t / FPS < 5.0f; ++t) {
+  for (int t = 0; t / FPS < 10.0f; ++t) {
     std::ostringstream os;
     os << std::setfill('0') << std::setw(5) << t;
     RenderInformation info{};
